c677b97e1d938416cd0160862d46f59704e68a39
Jennifer Gonzalez <kittys201@gmail.com>
2024-01-24 00:27:43 -0300

    fix

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -6,16 +6,16 @@ class Program
 
         while (playAgain)
         {
-      
+            // Generate a random number as the magic number
             Random randomGenerator = new Random();
             int magicNumber = randomGenerator.Next(1, 101);
 
             int guess = -1;
             int guessCount = 0;
 
-            Console.WriteLine("Welcome to the Super  Number game!");
+            Console.WriteLine("Welcome to the Guess My Number game!");
 
-            
+            // Keep looping until the user guesses the magic number
             while (guess != magicNumber)
             {
                 Console.Write("What is your guess? ");
@@ -36,15 +36,17 @@ class Program
                 }
             }
 
-          
+            // Display the number of guesses made
             Console.WriteLine($"Congratulations! You guessed the number in {guessCount} guesses.");
 
-           
-            Console.Write("Do you want to play again? (y/n): ");
+            // Ask if the user wants to play again
+            Console.Write("Do you want to play again? (yes/no): ");
             string playAgainResponse = Console.ReadLine().ToLower();
             
-            
+            // Check if the response starts with "y" to continue playing
             playAgain = playAgainResponse.StartsWith("y");
         }
 
-        Console.WriteLine("Thanks for playing! See you!!");
+        Console.WriteLine("Thanks for playing! Goodbye!");
+    }
+}
